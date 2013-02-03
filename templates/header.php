f10daa274163e3925885160489afa65d32633188
asharma2172 <akshay1227@gmail.com>
2013-02-03 00:33:55 -0500

    Update templates/header.php
    changed popover to tooltip

--- templates/header.php
+++ templates/header.php
@@ -15,37 +15,37 @@
         <script src="js/bootstrap.js"></script>
         <script>
         	$(document).ready(function () {
-        	$('#my_popover').popover();
+        	$('#my_popover').tooltip({placement: 'right'});
         	});
         </script>
           <script>
         	$(document).ready(function () {
-        	$('#my_popover1').popover();
+        	$('#my_popover1').tooltip({placement: 'right'});
         	});
         </script>
           <script>
         	$(document).ready(function () {
-        	$('#my_popover2').popover();
+        	$('#my_popover2').tooltip({placement: 'right'});
         	});
         </script>
           <script>
         	$(document).ready(function () {
-        	$('#my_popover3').popover();
+        	$('#my_popover3').tooltip({placement: 'right'});
         	});
         </script>
           <script>
         	$(document).ready(function () {
-        	$('#my_popover4').popover();
+        	$('#my_popover4').tooltip({placement: 'right'});
         	});
         </script>
           <script>
         	$(document).ready(function () {
-        	$('#my_popover5').popover();
+        	$('#my_popover5').tooltip({placement: 'right'});
         	});
         </script>
          <script>
         	$(document).ready(function () {
-        	$('#my_popover6').popover();
+        	$('#my_popover6').tooltip({placement: 'right'});
         	})
         	</script>
 
@@ -72,20 +72,20 @@
     <br><br>
 <ul class="nav nav-tabs nav-stacked">
 <h5>Stressed out? <br>Reach out.</h5>
-<li><a data-html="true" href="#" id="my_popover" title="Thayer Basement <br> <b>617-495-4969</b> <br> Nightly, 7pm - 7am <br> www.hcs.harvard.edu/~room13/" data-content="Room 13 is a dedicated group of upperclassmen who are here to talk about <b>anything<b> on your mind.">Room 13</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover1" title="HUHS 5th floor<br><b>617-495-7561</b><br> Nightly, 7pm - midnight <br> ww.hcs.harvard.edu/~pcc/" data-content="Peer Contraceptive Counselors is a group of undergraduate men and women who counsel Harvard students about contraceptives, sexually transmitted infections, relationships, and sexuality.">PCC</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover2" title="Lowell House Basement E 13<br><b>617-495-9600</b> <br>Hotline: Nightly, 9pm - 8am <br> Drop-in: Sun. - Thu. 9pm - midnight<br>www.hcs.harvard.edu/response/" data-content="Respone staffers are trained primarily in issues surrounding relationships, sexual assault, harassment, and abuse. However, they welcome calls from anyone who needs to talk, and from every gender and sexual orientation.">Response</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover3" title="Quincy House, F-Entry Basement<br><b>617-495-8200</b><br>Hotline: Nightly, 8pm - 8 am<br>Drop-in: Sun.-Thu., 8 - 11pm<br>www.hcs.harvard.edu/~echo/" data-content="ECHO is a peer counseling group that addresses concerns surrounding eating, body image, and self-esteem. ECHO staff members are undergraduates trained to provide non-judgmental support, both for those dealing with these issues and those who are concerned about a friend, roommate, significant other, or family member.">ECHO</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover4" title="Thayer Basement<br><b>617-495-8111</b><br>Hotline and drop-in: Thu.-Sun., 8pm - 1am<br> www.digitas.harvard.edu/~contact" data-content="Contact staff members share a deep concern about issues of sexual orientation, sex, sexuality and relationships and are interested in discussion of these topics. They also maintain a rapidly growing library of books, pamphlets, news clippings, and magazines. Drops-ins are welcome to peruse materials in the office or to borrow them from the library.">Contact</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover5" title="HUHS at Holyoke Center 4th Floor<br><b>617-495-2042</b>" data-content="Mental Health Services (UHS) offers Harvard students comprehensive outpatient care for a wide variety of mental health concerns, such as anxiety, depression, stress/crisis management, transitional issues, grief, and eating, sexual or relationship concerns.">Mental Health Services</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover6" title="5 Linden Street Cambridge, MA 02138 <br><b>617-495-2581</b><br>Mon.-Fri.,8:30am-5:30pm<br>www.bsc.harvard.edu" data-content="The Bureau can help students draw upon their existing strengths and develop new ones in their efforts to live a life that feels true to the whole of who they are and honors what matters to them in life.">Bureau of Study Counsel</a>   
-</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.hcs.harvard.edu/~room13/" id="my_popover" title="Thayer Basement <br> <b>617-495-4969</b> <br> Nightly, 7pm - 7am <br> Room 13 is a dedicated group of upperclassmen who are here to talk about <b>anything<b> on your mind.">Room 13</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.hcs.harvard.edu/~pcc/" id="my_popover1" title="HUHS 5th floor<br><b>617-495-7561</b><br> Nightly, 7pm - midnight <br> Peer Contraceptive Counselors is a group of undergraduate men and women who counsel Harvard students about contraceptives, sexually transmitted infections, relationships, and sexuality.">PCC</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.hcs.harvard.edu/response/" id="my_popover2" title="Lowell House Basement E 13<br><b>617-495-9600</b> <br>Hotline: Nightly, 9pm - 8am <br> Drop-in: Sun. - Thu. 9pm - midnight<br> Respone staffers are trained primarily in issues surrounding relationships, sexual assault, harassment, and abuse. However, they welcome calls from anyone who needs to talk, and from every gender and sexual orientation.">Response</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.hcs.harvard.edu/~echo/" id="my_popover3" title="Quincy House, F-Entry Basement<br><b>617-495-8200</b><br>Hotline: Nightly, 8pm - 8 am<br>Drop-in: Sun.-Thu., 8 - 11pm<br> ECHO is a peer counseling group that addresses concerns surrounding eating, body image, and self-esteem. ECHO staff members are undergraduates trained to provide non-judgmental support, both for those dealing with these issues and those who are concerned about a friend, roommate, significant other, or family member.">ECHO</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.digitas.harvard.edu/~contact" id="my_popover4" title="Thayer Basement<br><b>617-495-8111</b><br>Hotline and drop-in: Thu.-Sun., 8pm - 1am<br> Contact staff members share a deep concern about issues of sexual orientation, sex, sexuality and relationships and are interested in discussion of these topics. They also maintain a rapidly growing library of books, pamphlets, news clippings, and magazines. Drops-ins are welcome to peruse materials in the office or to borrow them from the library.">Contact</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.harvardsmiles.org/?page_id=3182" id="my_popover5" title="HUHS at Holyoke Center 4th Floor<br><b>617-495-2042</b> Mental Health Services (UHS) offers Harvard students comprehensive outpatient care for a wide variety of mental health concerns, such as anxiety, depression, stress/crisis management, transitional issues, grief, and eating, sexual or relationship concerns.">Mental Health Services</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.bsc.harvard.edu" id="my_popover6" title="5 Linden Street Cambridge, MA 02138 <br><b>617-495-2581</b><br>Mon.-Fri.,8:30am-5:30pm<br> The Bureau can help students draw upon their existing strengths and develop new ones in their efforts to live a life that feels true to the whole of who they are and honors what matters to them in life.">Bureau of Study Counsel</a>   
+			</li>
 </ul>
 </div>
     <div class="span10">
@@ -114,20 +114,20 @@
     <br>
 <ul class="nav nav-tabs nav-stacked">
 <h5>Stressed out? <br> Reach out.</h5>
-<li><a data-html="true" href="#" id="my_popover" title="Thayer Basement <br> <b>617-495-4969</b> <br> Nightly, 7pm - 7am <br> www.hcs.harvard.edu/~room13/" data-content="Room 13 is a dedicated group of upperclassmen who are here to talk about <b>anything<b> on your mind.">Room 13</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover1" title="HUHS 5th floor<br><b>617-495-7561</b><br> Nightly, 7pm - midnight <br> ww.hcs.harvard.edu/~pcc/" data-content="Peer Contraceptive Counselors is a group of undergraduate men and women who counsel Harvard students about contraceptives, sexually transmitted infections, relationships, and sexuality.">PCC</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover2" title="Lowell House Basement E 13<br><b>617-495-9600</b> <br>Hotline: Nightly, 9pm - 8am <br> Drop-in: Sun. - Thu. 9pm - midnight<br>www.hcs.harvard.edu/response/" data-content="Respone staffers are trained primarily in issues surrounding relationships, sexual assault, harassment, and abuse. However, they welcome calls from anyone who needs to talk, and from every gender and sexual orientation.">Response</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover3" title="Quincy House, F-Entry Basement<br><b>617-495-8200</b><br>Hotline: Nightly, 8pm - 8 am<br>Drop-in: Sun.-Thu., 8 - 11pm<br>www.hcs.harvard.edu/~echo/" data-content="ECHO is a peer counseling group that addresses concerns surrounding eating, body image, and self-esteem. ECHO staff members are undergraduates trained to provide non-judgmental support, both for those dealing with these issues and those who are concerned about a friend, roommate, significant other, or family member.">ECHO</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover4" title="Thayer Basement<br><b>617-495-8111</b><br>Hotline and drop-in: Thu.-Sun., 8pm - 1am<br> www.digitas.harvard.edu/~contact" data-content="Contact staff members share a deep concern about issues of sexual orientation, sex, sexuality and relationships and are interested in discussion of these topics. They also maintain a rapidly growing library of books, pamphlets, news clippings, and magazines. Drops-ins are welcome to peruse materials in the office or to borrow them from the library.">Contact</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover5" title="HUHS at Holyoke Center 4th Floor<br><b>617-495-2042</b>" data-content="Mental Health Services (UHS) offers Harvard students comprehensive outpatient care for a wide variety of mental health concerns, such as anxiety, depression, stress/crisis management, transitional issues, grief, and eating, sexual or relationship concerns.">Mental Health Services</a>   
-</li>
-<li><a data-html="true" href="#" id="my_popover6" title="5 Linden Street Cambridge, MA 02138 <br><b>617-495-2581</b><br>Mon.-Fri.,8:30am-5:30pm<br>www.bsc.harvard.edu" data-content="The Bureau can help students draw upon their existing strengths and develop new ones in their efforts to live a life that feels true to the whole of who they are and honors what matters to them in life.">Bureau of Study Counsel</a>   
-</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.hcs.harvard.edu/~room13/" id="my_popover" title="Thayer Basement <br> <b>617-495-4969</b> <br> Nightly, 7pm - 7am <br> Room 13 is a dedicated group of upperclassmen who are here to talk about <b>anything<b> on your mind.">Room 13</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.hcs.harvard.edu/~pcc/" id="my_popover1" title="HUHS 5th floor<br><b>617-495-7561</b><br> Nightly, 7pm - midnight <br> Peer Contraceptive Counselors is a group of undergraduate men and women who counsel Harvard students about contraceptives, sexually transmitted infections, relationships, and sexuality.">PCC</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.hcs.harvard.edu/response/" id="my_popover2" title="Lowell House Basement E 13<br><b>617-495-9600</b> <br>Hotline: Nightly, 9pm - 8am <br> Drop-in: Sun. - Thu. 9pm - midnight<br> Respone staffers are trained primarily in issues surrounding relationships, sexual assault, harassment, and abuse. However, they welcome calls from anyone who needs to talk, and from every gender and sexual orientation.">Response</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.hcs.harvard.edu/~echo/" id="my_popover3" title="Quincy House, F-Entry Basement<br><b>617-495-8200</b><br>Hotline: Nightly, 8pm - 8 am<br>Drop-in: Sun.-Thu., 8 - 11pm<br> ECHO is a peer counseling group that addresses concerns surrounding eating, body image, and self-esteem. ECHO staff members are undergraduates trained to provide non-judgmental support, both for those dealing with these issues and those who are concerned about a friend, roommate, significant other, or family member.">ECHO</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.digitas.harvard.edu/~contact" id="my_popover4" title="Thayer Basement<br><b>617-495-8111</b><br>Hotline and drop-in: Thu.-Sun., 8pm - 1am<br> Contact staff members share a deep concern about issues of sexual orientation, sex, sexuality and relationships and are interested in discussion of these topics. They also maintain a rapidly growing library of books, pamphlets, news clippings, and magazines. Drops-ins are welcome to peruse materials in the office or to borrow them from the library.">Contact</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.harvardsmiles.org/?page_id=3182" id="my_popover5" title="HUHS at Holyoke Center 4th Floor<br><b>617-495-2042</b> Mental Health Services (UHS) offers Harvard students comprehensive outpatient care for a wide variety of mental health concerns, such as anxiety, depression, stress/crisis management, transitional issues, grief, and eating, sexual or relationship concerns.">Mental Health Services</a>   
+			</li>
+			<li><a rel="tooltip" data-html="true" animation="true" placement='right' trigger="hover" target="_blank" href="http://www.bsc.harvard.edu" id="my_popover6" title="5 Linden Street Cambridge, MA 02138 <br><b>617-495-2581</b><br>Mon.-Fri.,8:30am-5:30pm<br> The Bureau can help students draw upon their existing strengths and develop new ones in their efforts to live a life that feels true to the whole of who they are and honors what matters to them in life.">Bureau of Study Counsel</a>   
+			</li>
 </ul>
 </div>
     
